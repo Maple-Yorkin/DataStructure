cd29d6be4ca3cb922df9e0164b76800e3e18309a
Yorkin <1993163301@qq.com>
2025-03-17 09:42:56 +0800

    practice: 提交LC946注释

--- LeetCode/946.cpp
+++ LeetCode/946.cpp
@@ -8,17 +8,19 @@ public:
     stack<int> s;
     bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
     {
-        int now = 0;
-        int p = 0;
-        for (now = 0; now < pushed.size(); now++)
+        int now = 0;                              // 指向pushed的指针
+        int p = 0;                                // 指向popped的指针
+        for (now = 0; now < pushed.size(); now++) // 在pushed中遍历
         {
             s.push(pushed[now]);
-            while (!s.empty() && s.top() == popped[p])
+            while (!s.empty() && s.top() == popped[p]) // 每次压入元素就判断顶部是否与p的指向元素相同，只要有可以弹出的就进行弹出
             {
+                // 如果相同则p++，并弹出栈顶元素（说明可以有当前这个序列）
                 p++;
                 s.pop();
             }
         }
+        // 如果运行到最后可以让栈为空，也就是说可以有输出序列而栈中没有剩下的元素
         if (s.empty())
             return true;
         return false;
