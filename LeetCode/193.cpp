c242a2f2c240eb7e0042430c3e2be6b97f59f72e
Yorkin <1993163301@qq.com>
2025-03-21 18:56:48 +0800

    practice: 提交LC193

---
+++ LeetCode/193.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+        int ans = 0;
+        while (n != 0)
+        {
+            ans += (n % 2);
+            n /= 2;
+        }
+        return ans;
+    }
+};
