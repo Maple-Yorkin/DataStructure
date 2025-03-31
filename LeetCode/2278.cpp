1c99f8d4b6987be2f2cc6642ddcd1fa0e5330b78
Yorkin <1993163301@qq.com>
2025-03-31 15:01:19 +0800

    practice: 提交LC2278

---
+++ LeetCode/2278.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int percentageLetter(string s, char letter)
+    {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == letter)
+            {
+                count++;
+            }
+        }
+        return count * 100 / s.length();
+    }
+};
+int main() {}
