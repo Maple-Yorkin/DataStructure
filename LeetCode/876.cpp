42bb7ab0db539ccffad218c5cb46878619178bbb
Yorkin <1993163301@qq.com>
2025-04-05 11:54:01 +0800

    practice: 提交LC876

---
+++ LeetCode/876.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        if (head->next == NULL)
+            return head;
+
+        while (fast != NULL || fast->next != NULL)
+        {
+
+            slow = slow->next;
+            fast = fast->next;
+            if (fast->next == NULL)
+                return slow;
+            fast = fast->next;
+            if (fast->next == NULL)
+                return slow;
+        }
+        return slow;
+    }
+};
+int main()
+{
+}
